99ecb6f147d50296766caf6234310002ec8e48eb
Matiran <maayan.tiran@gmail.com>
2025-02-04 17:31:17 +0200

    prod version 1.0.0

--- Online-Coding-Web-Application/src/pages/CodeBlockPage.tsx
+++ Online-Coding-Web-Application/src/pages/CodeBlockPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import CodeMirror from '@uiw/react-codemirror';
 import socket from '../utils/socket';
@@ -6,30 +6,33 @@ import axios from 'axios';
 import { ICodeBlock } from '../types/codeBlock';
 import { debounce } from 'lodash';
 import styles from '../styles/pages/CodeBlockPage.module.scss';
+import { javascript } from '@codemirror/lang-javascript';
 
 const CodeBlockPage: React.FC = () => {
   const { id } = useParams<{ id: string }>(); // Get the code block ID from the URL
   const [code, setCode] = useState<string>('');
+  const [solution, setSolution] = useState<string>('')
   const [role, setRole] = useState<'mentor' | 'student'>();
   const [studentCount, setStudentCount] = useState<number>(0);
   const [isSolved, setIsSolved] = useState<boolean>(false);
   const [title, setTitle] = useState<string>();
-  
-  const solutionRef = useRef<string>('');
+
+  // const solutionRef = useRef<string>('');
   const navigate = useNavigate();
 
   useEffect(() => {
     // Fetch code block details (template and solution)
     axios.get<ICodeBlock>(`http://localhost:3000/api/code-blocks/${id}`)
-    .then((response) => {
-      setTitle(response.data.title)
-      setCode(response.data.template);
-      solutionRef.current = response.data.solution;
-    })
-    .catch((error => {
-      console.error('Failed to fetch code block:', error);
-      alert('Error fetching code block. Please try again later.');
-    }));
+      .then(async (response) => {
+        setTitle(response.data.title);
+        setCode(response.data.template);
+        setSolution(response.data.solution);
+        // solutionRef.current = response.data.solution;
+      })
+      .catch((error => {
+        console.error('Failed to fetch code block:', error);
+        alert('Error fetching code block. Please try again later.');
+      }));
 
     // Join the room
     socket.emit('join-room', id);
@@ -39,15 +42,19 @@ const CodeBlockPage: React.FC = () => {
       setRole(data);
     });
 
-     // Listen for student count updates
-     socket.on('student-count', (data: number) => {
+    // Listen for student count updates
+    socket.on('student-count', (data: number) => {
       setStudentCount(data);
     });
 
     // Listen for code updates
-    socket.on('code-update', (data: string) => {
+    socket.on('code-update', async (data: string) => {
       setCode(data);
-      setIsSolved(data === solutionRef.current);
+      setIsSolved(data === solution);
+    });
+
+    socket.on('redirect-lobby', () => {
+      navigate(`/`);
     });
 
     // Cleanup on unmount
@@ -55,7 +62,7 @@ const CodeBlockPage: React.FC = () => {
       socket.removeAllListeners();
       socket.emit('leave-room', id);
     };
-  }, [id]);
+  }, [id, solution, navigate]);
 
   const debouncedEmit = debounce((roomId: string, newCode: string) => {
     socket.emit('code-update', { roomId, code: newCode });
@@ -64,19 +71,19 @@ const CodeBlockPage: React.FC = () => {
   const handleCodeChange = (newCode: string) => {
     if (role === 'student') {
       setCode(newCode);
-      if(id !== undefined) {
-        debouncedEmit( id , newCode);
-      }  
+      if (id !== undefined) {
+        debouncedEmit(id, newCode);
+      }
     }
   };
 
   return (
     <>
-      <div 
-        className={styles.pageHeader} 
+      <div
+        className={styles.pageHeader}
         onClick={() => navigate(`/`)}>
-          <label>Code Block Page</label>
-          <label>{title}</label>
+        <label>Code Block Page</label>
+        <label>{title}</label>
       </div>
       <div className={styles.stats}>
         <label>Participants: {studentCount}</label>
@@ -86,6 +93,7 @@ const CodeBlockPage: React.FC = () => {
         value={code}
         onChange={handleCodeChange}
         readOnly={role === 'mentor'}
+        extensions={[javascript()]}
         className="border rounded"
         height="50vh"
       />

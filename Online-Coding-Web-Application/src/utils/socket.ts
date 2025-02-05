51f980c08b985ea83dbef8f532639f0a49b177d0
Matiran <maayan.tiran@gmail.com>
2025-02-05 20:07:26 +0200

    change url for prod test

--- Online-Coding-Web-Application/src/utils/socket.ts
+++ Online-Coding-Web-Application/src/utils/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
-// const apiUrl = import.meta.env.VITE_PROD_API_URL;
-const apiUrl = import.meta.env.VITE_DEV_API_URL;
+const apiUrl = import.meta.env.VITE_PROD_API_URL;
+// const apiUrl = import.meta.env.VITE_DEV_API_URL;
 
 const SOCKET_URL = apiUrl; // Replace with your server URL
 const socket: Socket = io(SOCKET_URL);
